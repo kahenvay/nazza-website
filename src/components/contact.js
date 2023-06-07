057a02158796f984ab3632f4cb8d1d33a415dbe4
Ulysse Coates <u.coates@gmail.com>
2023-06-07 11:13:06 +0100

    changed image for more highlighted belgium

--- src/components/contact.js
+++ src/components/contact.js
@@ -58,7 +58,7 @@ export default function Contact() {
         target="_blank"
         href="https://www.google.com/maps/place/Chau.+de+Waterloo+1006,+1180+Uccle,+Belgium/@50.8009555,4.3717523,17z/data=!3m1!4b1!4m6!3m5!1s0x47c3c51f3a2012db:0x9899e34bac24c1a4!8m2!3d50.8009555!4d4.3743272!16s%2Fg%2F11csmv3lvv"
       >
-        <StaticImage src="../images/static-map-long.jpeg" />
+        <StaticImage src="../images/static-map-long-think-belgium.jpg" />
       </Link>
     </div>
   )
