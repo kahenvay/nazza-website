f096f6681409d9b4597b3036b760498f19847395
Ulysse Coates <u.coates@gmail.com>
2023-06-14 11:22:53 +0100

    lil fix for landing page video responsiveness

--- src/components/landingPage.css.ts
+++ src/components/landingPage.css.ts
@@ -1,9 +1,9 @@
 import { style } from "@vanilla-extract/css"
 
 export const video = style({
-    width: "100%",
-    height: "100%",
-    objectFit: "cover"
+  objectFit: "cover",
+  width: "100vw",
+  height: "100vw",
   })
 
 export const button = style({
